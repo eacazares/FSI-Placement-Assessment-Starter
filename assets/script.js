d44653a89fb3f1ee8533ba5d1213b882db0b3a4c
eacazares <eacazar@gmail.com>
2021-12-02 21:39:40 -0800

    final

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,52 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
+function updateQuantity(displayQuantity) {
+    let gbCookies= document.querySelector('#qty-gb')
+    gbCookies.textContent = displayQuantity
+}
 
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
 
-// We'll use these variables to track the counts of each cookie type
-let gb = 0      // Gingerbread
-let cc = 0      // Chocolate Chip
-let sugar = 0   // Sugar Sprinkle
+
+
+let yourName = "Erik Cazares" // HINT: Replace this with your own name!
 
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
+// We'll use these variables to track the counts of each cookie type
+let gb = 0     // Gingerbread
+let cc = 0      // Chocolate Chip
+let sugar = 0   // Sugar Sprinkle
+let total = gb + cc + sugar
 
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+let totalCookies = document.querySelector('#qty-total')
+totalCookies.getAttribu= `Total ${total}`
+
+//buttons
+const gbUp = document.querySelector('#add-gb')
+const gbDown = document.querySelector('#minus-gb')
+
+
+
+// Event listeners to add/subtract gingerbread cookies
+gbUp.addEventListener('click', function(e){
+    gb++
+    updateQuantity(gb)
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+gbDown.addEventListener('click', function(e){
+    if (gb > 0) {
+    gb--
+    }
+    updateQuantity(gb)
+})
+
+// Event listeners to add/subtract chocolate chip cookies
+document.getElementById('add-cc').addEventListener('click', function() {
+    console.log('cc button was clicked')
+})
+
+
+// Event listeners to add/subtract sugar sprinkle
+document.getElementById('add-sugar').addEventListener('click', function() {
+    console.log('sugar button was clicked')
+})
+
